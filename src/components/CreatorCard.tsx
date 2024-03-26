f1734b5cce5b07256e7271b8981e63023de84a94
Carlos <carlosvenegass8c@gmail.com>
2024-03-26 16:01:37 -0300

    added npm install --save-dev @babel/plugin-proposal-private-property-in-object --legacy-peer-deps

--- src/components/CreatorCard.tsx
+++ src/components/CreatorCard.tsx
@@ -10,7 +10,7 @@ function CreatorCard(props: Props) {
         alt="El todo poderoso gatito negro con zeus"
         className="rounded-full max-w-8 max-h-8 border"
       />
-      <p className="px-3 text-textColor text-lg font-light leading-relaxed font-Outfit">Creation of 
+      <p className="px-3 text-textColor text-lg font-light leading-relaxed font-outfit">Creation of 
         <a 
         href="#"
         className="text-white focus:outline-none focus:border-dotted focus:border-2 focus:border-priceColor hover:text-priceColor text-xl "
